1b8b7ffc13323c16abb57f359e4ad4f6cf086877
Amina Javed <61292170+amjav@users.noreply.github.com>
2020-11-16 19:26:06 +0000

    lab 2 work

--- js/main.js
+++ js/main.js
@@ -34,6 +34,68 @@
         //tell us the event that occured and much more
         console.dir(ev);
     }
+
+    //this is one way of changing the background using selectors
+    // document.querySelector(".red").addEventListener("click", function(){
+    //     document.querySelector("body").setAttribute("class","redBack")
+    // })
+
+    //node list generated
+    var colourButtons = document.querySelectorAll(".colPicker")
+
+    //node list is looped around and attaches events to everything in the 
+    //list.
+    //it calls the same function by exposing the same object
+    for(var i=0; i< colourButtons.length; i++){
+        colourButtons[i].addEventListener("click", chgColour)
+    }
+
+
+    //work out which one was clicked on and apply the class of colour
+    function chgColour(ev){
+        console.info(ev.target.classList[0])
+        var colourPicked = ev.target.classList[0] + "Back"
+        var bodyElement = document.querySelector("body")
+        if(colourPicked === "resetBack"){
+            bodyElement.removeAttribute("class")
+        }else{
+            bodyElement.setAttribute("class", colourPicked)
+        }
+    }
+
+    //image rotator
+    var imageAr = ['images/view1.jpg',
+                    'images/view2.jpg',
+                    'images/view3.jpg',
+                    'images/view4.jpg', 
+                    'images/view5.jpg', 
+                    'images/view6.jpg'
+    ];
+
+    
+    var counter = 0;
+
+    //makes sure an image is there straight away
+    chgImage();
+
+    setInterval(chgImage(), 1000)
+    
+    function chgImage(){
+        
+        counter++
+        if (counter === imageAr.length){
+             counter = 0;
+        }
+        document.getElementById("myImages").setAttribute("src", imageAr[counter])  
+    }
+
+    //console.dir(window.location.href);
+
+    var url = window.location.href
+    //console.dir(url.split("/")).pop();
+
+    var filename = url.split("/").pop();
+    console.info(filename)
     
 })();
 
